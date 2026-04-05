a1b2c65af7f1f4a11711f9b6ef50327570090768
Mohammedrahil2653 <mohmmedrahil07@gmail.com>
2026-04-05 23:05:58 +0530

    started loops--switch statement

---
+++ Loops/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            int switchvalue=2;
+            switch (switchvalue) {
+                case 1:
+                    System.out.println("the value is " + switchvalue);
+                    break;
+                case 2:
+                    System.out.println("the value is " + switchvalue);
+                    //break; //remove break it will continue till another break
+                case 3: case 4: case 5:
+                    System.out.println("the value may be " + switchvalue);
+                    break;
+                default:
+                    System.out.println("was not 1,2,3,4,5");
+                    break;
+            }
+    }
+}
