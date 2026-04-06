e1ea87aef584654217bc9f94bd59287b9ffd4823
Mohammedrahil2653 <mohmmedrahil07@gmail.com>
2026-04-06 20:24:28 +0530

    ENhanced switch

--- Loops/src/Main.java
+++ Loops/src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
             int switchvalue=2;
+            int switchvalue2=4;
             switch (switchvalue) {
                 case 1:
                     System.out.println("the value is " + switchvalue);
@@ -17,5 +18,14 @@ public class Main {
                     System.out.println("was not 1,2,3,4,5");
                     break;
             }
+
+        switch (switchvalue2) {
+            case 1 -> System.out.println("the value is " + switchvalue2);
+
+            case 2 -> System.out.println("the value is " + switchvalue2);
+            //break; //remove break it will continue till another break
+            case 3, 4, 5 -> System.out.println("the value may be " + switchvalue2);
+            default -> System.out.println("was not 1,2,3,4,5");
+        }
     }
 }
